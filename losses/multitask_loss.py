1fb71e27c250933177e2345b6e1483f18d98cddc
frizwankhan <f.mohamedrizwan@gmail.com>
2024-04-17 00:12:47 +0530

    distillation los

--- losses/multitask_loss.py
+++ losses/multitask_loss.py
@@ -15,3 +15,32 @@ class MultiTaskLoss(nn.Module):
         out['total'] = torch.sum(torch.stack([self.loss_weights[t] * out[t] for t in self.tasks]))
 
         return out
+    
+class MultiTaskDistillationLoss(nn.Module):
+    def __init__(self, tasks: list, loss_ft: nn.ModuleDict, loss_weights: dict):
+        super().__init__()
+        self.tasks = tasks
+        self.loss_ft = loss_ft
+        self.loss_weights = loss_weights
+
+    
+    def forward(self, pred, gt):
+        total = 0.
+        out = {}
+
+        # Losses initial task predictions (deepsup)
+        for task in self.tasks:
+            loss_ = self.loss_ft[task](pred[task], gt[task])
+            out['initial_%s' %(task)] = loss_
+            total += self.loss_weights[task] * loss_
+
+        # Losses at output  
+        for task in self.tasks:
+            pred_, gt_ = pred[task], gt[task]
+            loss_ = self.loss_ft[task](pred_, gt_)
+            out[task] = loss_
+            total += self.loss_weights[task] * loss_
+
+        out['total'] = total
+
+        return out
